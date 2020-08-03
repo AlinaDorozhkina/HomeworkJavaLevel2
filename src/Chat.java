e027b349672bb54e4e475dab3915dbb97aee254f
Alina_D <alinadorozhkina91@mail.ru>
2020-08-03 10:08:23 +0300

    домашка - клиентская часть чата

---
+++ src/Chat.java
@@ -0,0 +1,70 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Chat extends JFrame {
+    public Chat(String title) {
+        setTitle(title);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(100, 100, 500, 600);
+
+
+        JPanel jpanel = new JPanel(new BorderLayout());
+
+        JTextArea textArea = new JTextArea();
+        textArea.setEditable(false);
+        textArea.setForeground(new Color(29, 106, 4));
+        textArea.setFont(new Font("Verdana", Font.ITALIC, 18));
+        textArea.setBackground(new Color(209, 231, 188));
+        JScrollPane scroll = new JScrollPane(textArea);
+        jpanel.add(scroll);
+
+        add(jpanel);
+
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.setPreferredSize(new Dimension(50, 50));
+        JTextField textField = new JTextField();
+        textField.addActionListener(new ActionListener() {
+            private StringBuilder sb = new StringBuilder();
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (textField.getText().isBlank()) {
+                    return;
+                }
+                sb.append(textArea.getText()).append("\n").append(textField.getText());
+                textArea.setText(sb.toString());
+                textField.setText("");
+                sb.setLength(0);
+            }
+        });
+        panel.add(textField);
+
+
+        JButton sendButton = new JButton("SEND");
+        sendButton.setBackground(new Color(118, 227, 73));
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                StringBuilder sb = new StringBuilder();
+                if (textField.getText().isBlank()) {
+                    return;
+                }
+                sb.append(textArea.getText()).append("\n").append(textField.getText());
+                textArea.setText(sb.toString());
+                textField.setText("");
+                sb.setLength(0);
+            }
+        });
+
+        panel.add(sendButton, BorderLayout.EAST);
+
+        add(panel, BorderLayout.SOUTH);
+
+
+        setVisible(true);
+    }
+
+}

7f26717f3186af406c47a06f9fd10c7ee907acf4
Alina_D <alinadorozhkina91@mail.ru>
2020-08-19 02:06:54 +0300

    домашка 8

--- src/ru/geekbrains/java/homeworks/Main.java
+++ src/ru/geekbrains/java/homeworks/Main.java
@@ -1,8 +1,30 @@
 package ru.geekbrains.java.homeworks;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        TwoLinkedList cities = new TwoLinkedList();
+        cities.addLast("Chicago");
+        cities.addLast("Budapest");
+        cities.addLast("Rio");
+        System.out.println(cities);
+        cities.displayList();
+        cities.addFirst("Moscow");
+        System.out.println(cities);
+        cities.displayList();
+        System.out.println(cities.getSize());
+        cities.addByIndex(2, "Paris");
+        cities.displayList();
+        cities.addByIndex(0, "Roma");
+        System.out.println(cities);
+        cities.displayList();
+        System.out.println(cities.getSize());
+        System.out.println(cities);
+        cities.removeFirst();
+        cities.displayList();
+        cities.removeLast();
+        cities.displayList();
+        System.out.println(cities.getSize());
+        cities.addByIndex(10, "Miami");
+
     }
 }
